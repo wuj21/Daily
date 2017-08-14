cd30f59dea1504e1fa22b2b7cddd7f80a37a4ad8
Jack WU <jwu@pivotal.io>
2017-08-14 14:08:48 +0800

    b

--- go/src/gg/gg.go
+++ go/src/gg/gg.go
@@ -16,33 +16,36 @@ func judge(i, j, k uint64) {
 }
 
 func gg1(i, j uint64) {
-	var k, t uint64
-	if i*i/2 < EDGE {
-		t = i*i/2 + 2
-	} else {
-		t = EDGE
-	}
-	for k = j + 1; k <= t; k++ {
+	var k uint64
+	/*
+		if i*i/2 < EDGE {
+			t = i*i/2 + 2
+		} else {
+			t = EDGE
+		}
+	*/
+	for k = j + 1; k <= i*i/2+3; k++ {
 		judge(i, j, k)
 	}
 }
 
 func gg2(i uint64) {
 	//	fmt.Printf("Trying %d\n", i)
-	var j, t uint64
-	if i*i/2 < EDGE {
-		t = i*i/2 + 2
-	} else {
-		t = EDGE
-	}
-	for j = i + 1; j < t; j++ {
+	var j uint64
+	/*
+		if i*i/2 < EDGE {
+			t = i*i/2 + 2
+		} else {
+			t = EDGE
+		}*/
+	for j = i + 1; j < i*i/2+2; j++ {
 		gg1(i, j)
 	}
 }
 
 func main() {
 	var i uint64
-	for i = 1; i < EDGE-1; i++ { /*
+	for i = 1; i < EDGE+1; i++ { /*
 			for j = i + 1; j < EDGE; j++ {
 				for k = j + 1; k <= EDGE; k++ {
 					if i*i+j*j == k*k {
@@ -53,5 +56,5 @@ func main() {
 		go gg2(i)
 	}
 	fmt.Printf("%s\n", "I am here")
-	time.Sleep(5 * time.Minute)
+	time.Sleep(10 * time.Minute)
 }
