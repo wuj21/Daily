f1970cc60d603f1cac5e95588b842f6ebdaad016
Jack WU <maingalaxy@gmail.com>
2018-11-05 07:08:40 +0000

    list fonts

---
+++ java/java-learning/ListFonts.java
@@ -0,0 +1,10 @@
+import java.awt.GraphicsEnvironment;
+ 
+public class ListFonts {
+    public static void main(String args[]) {
+        GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        for(String font:e.getAvailableFontFamilyNames()) {
+            System.out.println(font);
+        }
+    }
+}
